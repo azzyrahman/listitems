508516acdbfc55328ec0b81eced9f3215e91181c
Azizur Rahman <arahman@mac-dan.corp.theweather.com.au>
2013-11-19 06:28:55 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Listitems::Application.config.secret_key_base = '1cecbdb5fcb27dec66c33e86ec54a99473afeb476070c44d6dbed06654b91c5057ed2398fc4c454f7e160b4e680432323fbe3c7238e36ab6f7a132b64b566a9e'
